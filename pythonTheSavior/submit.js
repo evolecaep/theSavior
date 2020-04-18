b1049e9b3b45a957bca383d7f050c7ad6efa2862
evolecaep <63893256+evolecaep@users.noreply.github.com>
2020-04-18 15:57:03 +0900

    Add files via upload

---
+++ pythonTheSavior/submit.js
@@ -0,0 +1,50 @@
+var ebtn = {};
+var pbtn = {};
+var abtn = {};
+
+ebtn=document.getElementById('economy_submit');
+pbtn=document.getElementById('politics_submit');
+abtn=document.getElementById('art_submit');
+
+ebtn.addEventListener('click', saveEconomyAnswer);
+pbtn.addEventListener('click', savePoliticsAnswer);
+abtn.addEventListener('click', saveArtAnswer);
+
+function saveEconomyAnswer(){
+  let message;
+  message=document.getElementById('economy_answer').value;
+  let ok;
+  ok=confirm('대답하시겠습니까?');
+  localStorage.a1=message;
+  if(ok){
+    location.href='introduction.html';
+    sessionStorage.b1='true';
+  } else {
+  }
+}
+
+function savePoliticsAnswer(){
+  let message;
+  message=document.getElementById('politics_answer').value;
+  let ok;
+  ok=confirm('대답하시겠습니까?');
+  localStorage.a2=message;
+  if(ok){
+    location.href='introduction.html';
+    sessionStorage.b2='true';
+  } else {
+  }
+}
+
+function saveArtAnswer(){
+  let message;
+  message=document.getElementById('art_answer').value;
+  let ok;
+  ok=confirm('대답하시겠습니까?');
+  localStorage.a3=message;
+  if(ok){
+    sessionStorage.b3='true';
+    location.href='introduction.html';
+  } else {
+  }
+}
